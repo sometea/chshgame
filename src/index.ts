11671506b598729341b9bd44182afc2239a800a7
David Dasenbrook <david.dasenbrook@tngtech.com>
2018-03-01 19:40:26 +0100

    assign Alice and Bob their own sockets

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { createServer } from 'net';
+import { createServer, Socket } from 'net';
+
+let socketAlice: Socket, socketBob: Socket;
 
 const server = createServer(socket => {
-    socket.write('Welcome to the CHSH game!');
+    socket.write('Welcome to the CHSH game!\n');
+    if (!socketAlice) {
+        socketAlice = socket;
+        socket.write('You are Alice!\n');
+        return;
+    }
+    if (!socketBob) {
+        socketBob = socket;
+        socket.write('You are Bob!\n');
+        return;
+    }
+    socket.write('Unfortunately, there are already two players. Bye!\n')
+    socket.destroy();
 }).listen(8000);
