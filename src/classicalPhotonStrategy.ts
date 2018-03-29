4476c45ff726a342cdd043a21e315f513ce6b282
David Dasenbrook <david.dasenbrook@tngtech.com>
2018-03-29 20:15:48 +0200

    implement classical photon strategy that listens on experiment port 8001

--- src/classicalPhotonStrategy.ts
+++ src/classicalPhotonStrategy.ts
@@ -7,6 +7,12 @@ export class ClassicalPhotonStrategy implements GameStrategy {
 
     answer(question: Bit): Promise<Bit> {
         this.connection.write(question.toString() + '\n');
-        return Promise.resolve(new Bit());
+        return new Promise<Bit>((resolve, reject) => {
+            this.connection.on('data', data => {
+                if (data.toString().includes('Result:')) {
+                    resolve((new Bit()).fromString(data.toString().substr(-1)));
+                }
+            })
+        });
     }
 }
--- src/client.ts
+++ src/client.ts
@@ -2,19 +2,22 @@ import { createConnection, Socket } from "net";
 import { GameStrategy } from "./gameStrategy";
 import { DeterministicStrategy } from "./deterministicStrategy";
 import { Bit } from "./bit";
+import { ClassicalPhotonStrategy } from "./classicalPhotonStrategy";
 
-const connection = createConnection(8000);
+const gameConnection = createConnection(8000);
+const experimentConnection = createConnection(8001);
 
 let counter = 0;
 const maxCounter = 500;
 
-const strategy: GameStrategy = new DeterministicStrategy();
+// const strategy: GameStrategy = new DeterministicStrategy();
+const strategy: GameStrategy = new ClassicalPhotonStrategy(experimentConnection);
 
-connection.on('data', async data => {
+gameConnection.on('data', async data => {
     if (data.toString().includes('Referee question') && counter < maxCounter) {
         const question = (new Bit()).fromString(data.toString().substr(-1));
         const answer = await strategy.answer(question);
-        connection.write(answer.toString());
+        gameConnection.write(answer.toString());
         counter++;
     }
     console.log(data.toString());
