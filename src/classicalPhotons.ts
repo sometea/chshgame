a2e790cd00f4befce5478d5bed4abebd76c72f46
David Dasenbrook <david.dasenbrook@gmail.com>
2018-11-17 12:38:45 +0100

    improve classical photon measuring strategy

--- src/classicalPhotons.ts
+++ src/classicalPhotons.ts
@@ -3,9 +3,20 @@ import { Bit } from "./bit";
 
 export class ClassicalPhotons implements SharedPhotons {
     measure(setting1: Bit, setting2: Bit): [Bit, Bit] {
-        const bitZero = (new Bit()).fromNumber(0);
-        const bitOne = (new Bit()).fromNumber(1);
+        const bitZero = new Bit(0);
+        const bitOne = new Bit(1);
+        let output1: Bit;
+        let output2: Bit;
+        [output1, output2] = Math.random() < 0.5 ? [bitZero, bitOne] : [bitOne, bitZero];
 
-        return Math.random() < 0.5 ? [bitZero, bitOne] : [bitOne, bitZero];
+        if (!setting1.toBoolean()) {
+            output1 = new Bit(0);
+        }
+
+        if (!setting2.toBoolean()) {
+            output2 = new Bit(0);
+        }
+
+        return [output1, output2];
     }
 }
